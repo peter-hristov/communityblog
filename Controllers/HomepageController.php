4bee9f4e9506689092585be57b86b6b052d1c48f
famouscake <peter.g.hristov@gmail.com>
2014-05-20 10:07:29 +0300

    asdf

--- Controllers/HomepageController.php
+++ Controllers/HomepageController.php
@@ -5,16 +5,8 @@ require 'Controller.php';
 class HomepageController extends Controller{
 
 
-    public $a = 10;
-
     public function index()
     {
-
-
-
-
-
-
         echo $this->renderView('homepage');
     }
 }
