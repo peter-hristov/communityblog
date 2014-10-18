cfa4aae147fb27eaab95dd4dd449d0bf95b70f44
Famous <peter.g.hristov@gmail.com>
2014-10-18 16:34:56 +0100

    changed cofig to production

--- init.php
+++ init.php
@@ -6,12 +6,12 @@ if (!defined("__DIR__")) define("__DIR__", dirname(__FILE__));
 if (!defined("__ROOT__")) define("__ROOT__", __DIR__);
 if (!defined("__APPNAME__")) define("__APPNAME__", array_pop(explode('/', __ROOT__)));
 
-if (!defined("__SITENAME__")) define("__SITENAME__", 'partyplant.dev');
+if (!defined("__SITENAME__")) define("__SITENAME__", 'partyplant.eu');
 
 
 
 // This is either dev.{developer handle} or just production
-if (!defined("__ENVIRONMENT__")) define("__ENVIRONMENT__", 'dev.peter');
+if (!defined("__ENVIRONMENT__")) define("__ENVIRONMENT__", 'production');
 
 
 // Autoloader setup for core framework files
