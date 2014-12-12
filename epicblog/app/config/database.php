9b817e4467146a16869100f4dd54e2e3f2dbce26
Famous <peter.g.hristov@gmail.com>
2014-12-12 08:48:17 +0000

    getting stuff rdy for preduction

--- epicblog/app/config/database.php
+++ epicblog/app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'epicblog',
 			'username'  => 'root',
-			'password'  => '123',
+			'password'  => 'rcdudepass1',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
