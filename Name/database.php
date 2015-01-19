5cdeae73f00fa362d752f048496d4fa5044db93e
peter <peter@SmallCake.dlink.com>
2015-01-19 00:20:19 +0000

    fdsf

--- Name/database.php
+++ Name/database.php
@@ -1,15 +1,15 @@
-<?php 
+<?php
 
 
 function getPDO() {
 
 	try {
-		
-		$db = array("host"=>"localhost", "name"=>"name", "user"=>"root", "password"=>"");
+
+		$db = array("host"=>"localhost", "name"=>"name", "user"=>"root", "password"=>"rcdudepass1");
 		$connection = new PDO('mysql:host=' . $db['host'] . '; dbname=' . $db['name'], $db['user'], $db['password']);
 		$connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 		$connection->exec("SET CHARACTER SET utf8");
-		
+
 		}
 	catch(PDOException $err) {
 		echo "THE CONNECTION HAS FAILED : ";
